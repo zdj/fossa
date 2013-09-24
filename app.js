eb9abacf4feb30d0462d65b4e5683e10dec5e5dd
Zack Jones <zjones@ctacorp.com>
2013-09-24 18:09:53 -0400

    Better config

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var conf = require('./lib/config.json');
-var colors = require('colors');
+var fossa = require('./lib/fossa');
 
 var app = express();
 
@@ -44,61 +43,7 @@ var server = http.createServer(app);
 
 app.get('/', routes.index);
 
-conf['services'].forEach(function (service) {
-    console.log(('Creating service listener: ' + service['name']).blue);  
-	var description = service['description']; 
-	if(description) {
-		console.log(('  Description: ').white + description.cyan);  		
-	} 
-    var path = service['path'];
-    console.log(('  Path: ').white + path.yellow);
-    var responseType = service['response']['type'].toLowerCase();
-    console.log(('  Response Type: ').white + responseType.yellow);    
-
-    function sendResponse(req, res, data) {
-		var contentType = req.header("Content-Type");
-		if(contentType) {			
-			res.setHeader('Content-Type', contentType);
-		} else {
-			res.setHeader('Content-Type', 'text/plain');
-		}
-		if(data) {			
-			res.setHeader('Content-Length', data.length);	
-		}
-        res.send(data);
-    }
-	
-	console.log(("  Types:").white + JSON.stringify(service['types']).toUpperCase().yellow);
-	
-	service['types'].forEach(function(type) {
-		
-		type = type.toUpperCase();
-
-	    if (type == 'GET') {
-		    var response = require('./lib/scripts/' + responseType).get;
-	        app.get(path, function (req, res) {
-	            response(req,res,sendResponse);
-	        });
-	    } else if (type == 'POST') {
-		    response = require('./lib/scripts/' + responseType).post;
-	        app.post(path, function (req, res) {
-	            response(req,res,sendResponse);
-	        });
-	    } else if (type == 'DELETE') {
-		    response = require('./lib/scripts/' + responseType).delete;
-	        app.delete(path, function (req, res) {
-	            response(req,res,sendResponse);
-	        });
-	    } else if (type == 'PUT') {
-		    response = require('./lib/scripts/' + responseType).put;
-	        app.put(path, function (req, res) {
-	            response(req,res,sendResponse);
-	        });
-	    }
-	});	 
-	
-	console.log();  
-});
+fossa.loadScripts(app);
 
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
