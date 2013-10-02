9be32495af3519571b7d129a4cf6824e9b3c38f7
Zack Jones <zjones@ctacorp.com>
2013-10-01 20:03:51 -0400

    Added tests for http service

--- test/test_service_http.js
+++ test/test_service_http.js
@@ -1,16 +1,84 @@
 var assert = require("assert");
-var fossa = require("../lib/services/http");
+var http = require("../lib/services/http");
 
 describe('Http Service Test', function() {
-	
+
 	describe('GET', function() {
-		it('should create 21 services', function(done) {
-			assert.equal(21, serviceCount);
-			assert.equal(4, _.keys(services['GET']).length);
-			assert.equal(7, _.keys(services['POST']).length);
-			assert.equal(7, _.keys(services['PUT']).length);
-			assert.equal(3, _.keys(services['DELETE']).length);
-			done();
+		it('should return HTTP 200 "Hello!"', function(done) {
+
+			http.get({}, {}, {
+				"statusCode": 200,
+				"response": "Hello!"
+			}, function(req, res, data, statusCode) {
+				assert.equal(200, statusCode);
+				assert.equal('"Hello!"', data);
+				done();
+			});
+		})
+	}),
+	describe('PUT', function() {
+		it('should return HTTP 204', function(done) {
+
+			http.put({}, {}, {
+				"statusCode": 204
+			}, function(req, res, data, statusCode) {
+				assert.equal(204, statusCode);
+				assert(!data);
+				done();
+			});
+		})
+	}),
+	describe('POST', function() {
+		it('should return HTTP 200 with JSON response', function(done) {
+
+			http.post({}, {
+				set: function(headerKey, headerValue) {
+					assert.equal('Content-Type', headerKey);
+					assert.equal('application/json; charset=UTF-8', headerValue);
+				}
+			}, {
+				"statusCode": 200,
+				"response": {
+					"response": "OK"
+				},
+				"contentType": "application/json; charset=UTF-8"
+			}, function(req, res, data, statusCode) {
+				assert.equal(200, statusCode);
+				assert.equal('{"response":"OK"}', data);
+				done();
+			});
+		})
+	}),
+	describe('DELETE', function() {
+		it('should return HTTP 404 "Not Found"', function(done) {
+
+			http.put({}, {}, {
+				"statusCode": 404,
+				"response": "Not Found"
+			}, function(req, res, data, statusCode) {
+				assert.equal(404, statusCode);
+				assert.equal('"Not Found"', data);
+				done();
+			});
+		})
+	}),
+	describe('GET with file response', function() {
+		it('should return HTTP 200 "Hello from a file!"', function(done) {
+
+			http.put({}, {
+				set: function(headerKey, headerValue) {
+					assert.equal('Content-Type', headerKey);
+					assert.equal('text/plain', headerValue);
+				}
+			}, {
+				"statusCode": 200,
+				"file": "hello.txt",
+				"contentType": "text/plain"
+			}, function(req, res, data, statusCode) {
+				assert.equal(200, statusCode);
+				assert.equal('Hello from a file!', new String(data));
+				done();
+			});
 		})
 	})
 })
