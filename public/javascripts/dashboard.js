7070e06cf4e0f962d63dc58dcfabf34a64626214
Zack Jones <zjones@ctacorp.com>
2013-11-12 19:14:49 -0500

    Start of dashboard overhaul

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,35 +1,8 @@
 $(document).ready(function () {
 
-    $("form[name|='http']").submit(function (event) {
-        var $form = $(this);
-        executeService(event,$form)
-    });
+    $('.nav li').click(function(){
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+        console.log(this);
+    })
 });
-
-function executeService(event, form) {
-
-    event.preventDefault();
-
-    var type = form.find("input[name='type']").val();
-    var method = form.find("input[name='method']").val();
-    var params = form.find("input[name='params']").val();
-    var contentType = form.find("input[name='contentType']").val();
-
-    var service = $.ajax({
-        url: "/service",
-        type: "GET",
-        data: {
-            type: type,
-            method: method,
-            params: params
-        }
-    }).done(function (data, textStatus, jqXHR) {
-        handleResponse(jqXHR);
-    }).fail(function (jqXHR) {
-        handleResponse(jqXHR);
-    });
-}
-
-function handleResponse(jqXHR) {
-    alert("Result was: " + jqXHR.responseText + "\n\nStatus Code: " + jqXHR.status);
-}
