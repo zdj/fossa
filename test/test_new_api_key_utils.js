931d4b1a2d2860a64353250f97de47a7e312c691
Zack Jones <zjones@ctacorp.com>
2014-01-03 10:12:09 -0500

    checking in new api key utils

--- test/test_new_api_key_utils.js
+++ test/test_new_api_key_utils.js
@@ -5,15 +5,16 @@ var apiKeyUtils = require('../lib/utils/new_api_key_utils');
 
 describe('New API Key Utils Tests', function () {
 
-    before(function (done) {
+    beforeEach(function (done) {
         fs.rename('lib/keys', 'lib/keys_old', function (err1) {
             fs.mkdir('lib/keys', function (err1) {
+                if (err1) throw err1;
                 done();
             });
         });
     });
 
-    after(function (done) {
+    afterEach(function (done) {
         fsExtra.remove('lib/keys', function (err1) {
             fs.rename('lib/keys_old', 'lib/keys', function (err1) {
                 fsExtra.remove('lib/keys_old', function (err1) {
@@ -40,6 +41,281 @@ describe('New API Key Utils Tests', function () {
                     assert.equal(keyAgreement.prime.length, 88);
                     done();
                 });
+            });
+        });
+    });
+
+    describe('get key agreement', function () {
+        it('should return the key agreement by entity1 name and entity2 public key', function (done) {
+            apiKeyUtils.createKeyAgreement('server', 'client', function(keyAgreement) {
+                apiKeyUtils.getKeyAgreement('server',keyAgreement.entity2PublicKey, function(_keyAgreement) {
+                    assert.equal(_keyAgreement.entity1PrivateKey, keyAgreement.entity1PrivateKey);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('perform authentication check', function () {
+        it("should not allow access if 'Authentication' header is missing the apiKey", function (done) {
+            var request = {
+                header: function (name) {
+                    return "beefaroni";
+                }
+            };
+            var response = {
+            };
+            var params = {
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if 'Authentication' header is missing", function (done) {
+            var request = {
+                header: function (name) {
+                    return null;
+                }
+            };
+            var response = {
+            };
+            var params = {
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey is in the wrong place", function (done) {
+            var request = {
+                header: function (name) {
+                    return " a_pee_eye_key";
+                }
+            };
+            var response = {
+            };
+            var params = {
+                "apiKey": "a_pee_eye_key"
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey value doesn't exist", function (done) {
+            var request = {
+                header: function (name) {
+                    return "a_pee_eye_key";
+                }
+            };
+            var response = {
+            };
+            var params = {
+                "apiKey": "a_pee_eye_key"
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey value isn't separated with a colon", function (done) {
+            var request = {
+                header: function (name) {
+                    return "a_pee_eye_key donkeykong";
+                }
+            };
+            var response = {
+            };
+            var params = {
+                "apiKey": "a_pee_eye_key"
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey value's public key is empty", function (done) {
+            var request = {
+                header: function (name) {
+                    return "a_pee_eye_key :kong";
+                }
+            };
+            var response = {
+            };
+            var params = {
+                "apiKey": "a_pee_eye_key"
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey value's hmac hash is empty", function (done) {
+            var request = {
+                header: function (name) {
+                    return "a_pee_eye_key donkey:";
+                }
+            };
+            var response = {
+            };
+            var params = {
+                "apiKey": "a_pee_eye_key"
+            };
+            var callback = function (req, res, message, statusCode) {
+                assert.equal(401, statusCode);
+                assert.equal('Not authorized', message);
+                done();
+            };
+            var methodToCall = function () {
+            };
+            apiKeyUtils.performAuthCheck(request, response, params, methodToCall, 'server', callback);
+        });
+        it("should not allow access if the apiKey value's public key doesn't match a private key", function (done) {
+
+            apiKeyUtils.createKeyAgreement('server','client', function(keyAgreement) {
+
+                var request = {
+                    header: function (name) {
+                        return "a_pee_eye_key donkey:kong";
+                    }
+                };
+                var response = {
+                };
+                var params = {
+                    "apiKey": "a_pee_eye_key"
+                };
+                var callback = function (req, res, message, statusCode) {
+                    assert.equal(401, statusCode);
+                    assert.equal('Not authorized', message);
+                    done();
+                };
+                var methodToCall = function () {
+                };
+                apiKeyUtils.performAuthCheck(request, response, params, methodToCall, keyAgreement.entity1, callback);
+            });
+        });
+        it("should not allow access if the hmac hashes don't match", function (done) {
+
+            apiKeyUtils.createKeyAgreement('server','client', function(keyAgreement) {
+                var data = JSON.stringify({
+                    "message": "whats up!"
+                });
+                var headers = {
+                    'Date': '     ' + new Date().toString(),
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length
+                };
+                var method = 'POST';
+
+                var canonicalizedHeaders = apiKeyUtils.getCanonicalizedHeaders(headers);
+                var canonicalizedResource = '/secure/logger';
+                var hmacHash = apiKeyUtils.createHmacHash(data, method, canonicalizedHeaders, canonicalizedResource, keyAgreement);
+                var publicKey = keyAgreement.entity2PublicKey;
+
+                var request = {
+                    header: function (name) {
+                        if(name=='Authorization') {
+                            return "a_pee_eye_key " + publicKey + ":" + hmacHash;
+                        } else {
+                            return null;
+                        }
+                    },
+                    headers: headers,
+                    method: method,
+                    url: '/secure/logger',
+                    rawBody: JSON.stringify({
+                        "message": "whats up dude!"
+                    })
+                };
+                var response = {
+                };
+                var params = {
+                    "apiKey": "a_pee_eye_key"
+                };
+                var callback = function (req, res, message, statusCode) {
+                    assert.equal(401, statusCode);
+                    assert.equal('Not authorized', message);
+                    done();
+                };
+                var methodToCall = function () {
+                };
+
+                apiKeyUtils.performAuthCheck(request, response, params, methodToCall, keyAgreement.entity1, callback);
+            });
+        });
+        it("should allow access if the hmac hashes match", function (done) {
+
+            apiKeyUtils.createKeyAgreement('server','client', function(keyAgreement) {
+
+                var data = JSON.stringify({
+                    "message": "whats up!"
+                });
+                var headers = {
+                    'Date': '     ' + new Date().toString(),
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length
+                };
+                var method = 'POST';
+
+                var canonicalizedHeaders = apiKeyUtils.getCanonicalizedHeaders(headers);
+                var canonicalizedResource = apiKeyUtils.getCanonizalizedResource('/secure/logger');
+                var hmacHash = apiKeyUtils.createHmacHash(data, method, canonicalizedHeaders, canonicalizedResource, keyAgreement);
+                var publicKey = keyAgreement.entity2PublicKey;
+
+                var request = {
+                    header: function (name) {
+                        if(name=='Authorization') {
+                            return "a_pee_eye_key " + publicKey + ":" + hmacHash;
+                        } else {
+                            return null;
+                        }
+                    },
+                    headers: headers,
+                    method: method,
+                    url: '/secure/logger',
+                    rawBody: data
+                };
+                var response = {
+                };
+                var params = {
+                    "apiKey": "a_pee_eye_key"
+                };
+                var callback = function (req, res, message, statusCode) {
+                    assert(false, "expected method was not called, got a " + statusCode + ": " + message + " instead");
+                };
+                var methodToCall = function (req, res, params, cb) {
+                    done();
+                };
+
+                console.log("\nBitch ass! \n");
+                console.log(keyAgreement);
+
+                apiKeyUtils.performAuthCheck(request, response, params, methodToCall, keyAgreement.entity1, callback);
             });
         });
     });
